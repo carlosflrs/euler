321c00063cb44db5087ae22ffab055b6a033b4bd
Carlos Flores <carlos.flrsarlln@gmail.com>
2014-12-16 16:47:40 -0800

    ARGS access.

--- Four.java
+++ Four.java
@@ -1,10 +1,11 @@
-/** Finds the largest palindrome made from the product of two 3-digit numbers.
+/** Finds the largest palindrome made from the product of two ARGS[0]-digit
+ * numbers.
  * @author Carlos A. Flores-Arellano.
  **/
 public class Four {
 
     public static void main (String[] args) {
-        System.out.println(largestPalindrome());
+        System.out.println(largestPalindrome(Integer.parseInt(args[0])));
     }
 
     private static boolean isPalindrome(String s) {
@@ -16,12 +17,12 @@ public class Four {
         return isPalindrome(s.substring(1, s.length() - 1));
     }
     
-    private static int largestPalindrome() {
+    private static int largestPalindrome(int limit) {
         int i;
         int j;
         int max = Integer.MIN_VALUE;
-        for (i = 999; i > 1; i = i - 1) {
-            for (j = 999; j > 1; j = j - 1) {
+        for (i = limit; i > 1; i = i - 1) {
+            for (j = limit; j > 1; j = j - 1) {
                 if (isPalindrome(String.valueOf(i * j))) {
                     max = Math.max(max, i * j);
                 }
--- Six.java
+++ Six.java
@@ -1,28 +1,28 @@
 /** Finds the difference between the sum of the squares of the first
- * one hundred natural numbers and the square of the sum.
+ * ARGS[0] natural numbers and the square of the sum.
  * @author Carlos A. Flores-Arellano.
  **/
 public class Six {
 
     public static void main(String[] args) {
-        System.out.println(difference());
+        System.out.println(difference(Integer.parseInt(args[0])));
     }
-    private static int sumOfSquares() {
+    private static int sumOfSquares(int x) {
         int i;
         int sum = 0;
-        for (i = 1; i <= 100; i = i + 1) {
+        for (i = 1; i <= x; i = i + 1) {
             sum = sum + (i * i);
         }
         return sum;
     }
 
-    private static int squareOfSum() {
+    private static int squareOfSum(int x) {
         int sum;
-        sum = (100 * 101) / 2;
+        sum = (x * (x + 1)) / 2;
         return sum * sum;
     }
 
-    private static int difference() {
-        return squareOfSum() - sumOfSquares();
+    private static int difference(int x) {
+        return squareOfSum(x) - sumOfSquares(x);
     }
 }
